fecf548c9c67e56749a661e32cbadeb144f752a9
TS <ts@ts-VPCEG1BFX.(none)>
2011-09-07 13:20:06 -0400

    commit about commit

--- greeter.rb
+++ greeter.rb
@@ -1,3 +1,11 @@
 #!/usr/bin/env ruby
 # This script will greet people and stuff
 
+class Greeter
+  def hello name
+    puts "Hello, #{name}/"
+  end
+end
+
+my_greeter = Greeter.new
+my_greeter.hello "Anthony"
