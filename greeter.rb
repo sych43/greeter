82bfca104bae7bcee01683c2ebc8f5b398d20179
TS <ts@ts-VPCEG1BFX.(none)>
2011-09-07 14:21:19 -0400

    commit about commit

--- greeter.rb
+++ greeter.rb
@@ -1,11 +1,47 @@
 #!/usr/bin/env ruby
 # This script will greet people and stuff
 
-class Greeter
-  def hello name
-    puts "Hello, #{name}/"
-  end
+
+
+module GreetingAndFarewell
+
+
+  def name
+    @name ||= "person who will give me money for service"
 end
 
-my_greeter = Greeter.new
-my_greeter.hello "Anthony"
+  def name new_name
+    @name = new_name
+end
+
+  def hello 
+    puts "Hello, #{@name}!"
+end
+
+  def farewell name
+    puts "Farewaell, #{@name}!"
+end
+
+class Greeter
+include GreetingAndFarewell
+
+  def initialize new_name = nil
+    @name = new_name
+end
+end
+
+class Customer
+include  GreetingAndFarewell
+
+def mame 
+@name ||="person who will provide me with a good or service"
+end
+end
+
+my_greeter = Greeter.new ARGV.first
+@name = ARGV.first
+my_greeter.hello
+my_greeter.farewell
+
+# call security
+# farewell
